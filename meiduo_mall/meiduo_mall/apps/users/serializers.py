4347c386d176235ef31d4b78a05f87795490ca7e
xingyue xing <Star_2424@126.com>
2019-01-13 11:23:10 +0800

    user center info: code about user address(add update...)

--- meiduo_mall/meiduo_mall/apps/users/serializers.py
+++ meiduo_mall/meiduo_mall/apps/users/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 from rest_framework_jwt.settings import api_settings
 from celery_tasks.email.tasks import send_verify_email
 
-from .models import User
+from .models import User, Address
 
 
 class CreateUserSerializer(serializers.ModelSerializer):
@@ -118,3 +118,35 @@ class EmailSerializer(serializers.ModelSerializer):
         # send email
         send_verify_email.delay(email, verify_url)
         return instance
+
+
+class UserAddressSerializer(serializers.ModelSerializer):
+    '''
+    user address serializer
+    '''
+    province = serializers.StringRelatedField(read_only=True)
+    city = serializers.StringRelatedField(read_only=True)
+    district = serializers.StringRelatedField(read_only=True)
+    province_id = serializers.IntegerField(label='province_id', required=True)
+    city_id = serializers.IntegerField(label='city_id', required=True)
+    district_id = serializers.IntegerField(label='district_id', required=True)
+
+    class Meta:
+        model = Address
+        exclude = ('user', 'is_deleted', 'create_time', 'update_time')
+
+
+    def validate_mobile(self, value):
+        if not re.match(r'^1[3-9]\d{9}$', value):
+            raise serializers.ValidationError('mobile format error')
+        return value
+
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
+
+
+class AddressTitleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = ('title',)
--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import mixins
+from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet
 
-
+from users import constants
 from . import serializers
 from users.models import User
 
@@ -95,4 +99,76 @@ class VerifyEmailView(APIView):
             return Response({'message': 'OK'})
 
 
+'''
+1. get addresses: GET /addresses    /addresses/<pk>
+2. add address: POST /addresses/
+3. update: PUT /addresses/<pk>/
+4. delete: DELETE /addresses/<pk>
+5. set default address: PUT /addresses/<pk>/status
+6. set address title: PUT /addresses/<pk>/title
+'''
+
+
+class AddressViewSet(mixins.CreateModelMixin, mixins.UpdateModelMixin, GenericViewSet):
+    '''
+    user addresses: add, update
+    '''
+    serializer_class = serializers.UserAddressSerializer
+    permissions = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.request.user.addresses.filter(is_deleted=False)
+
+    def list(self, request, *args, **kwargs):
+        '''
+        address list
+        '''
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        user = self.request.user
+        return Response({
+            'user_id': user.id,
+            'default_address_id': user.default_address_id,
+            'limit': constants.USER_ADDRESS_CPUNTS_LIMIT,
+            'addresses': serializer.data,
+        })
+
+    def create(self, request, *args, **kwargs):
+        count = request.user.addresses.count()
+        if count >= constants.USER_ADDRESS_CPUNTS_LIMIT:
+            return Response({'message': 'the count of the user address has achieved the limit'}, status=status.HTTP_400_BAD_REQUEST)
+
+        return super().create(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        address = self.get_object()
+        address.is_deleted = True
+        address.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(methods=['put'], detail=True)
+    def status(self, request, pk=None, address_id=None):
+        '''
+        set default address
+        :param request:
+        :param pk:
+        :param address_id:
+        :return:
+        '''
+        address = self.get_object()
+        request.user.default_address = address
+        request.user.save()
+        return Response({'message': 'OK'}, status=status.HTTP_200_OK)
+
+    @action(methods=['put'], detail=True)
+    def title(self, request, pk=None, address_id=None):
+        """
+        修改标题
+        """
+        address = self.get_object()
+        serializer = serializers.AddressTitleSerializer(instance=address, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
